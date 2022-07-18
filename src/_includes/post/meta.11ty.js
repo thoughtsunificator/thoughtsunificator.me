858408c6b4f3728956951c7a82e47ba4fb92e5e9
thoughtsunificator <oss@thoughtsunificator.me>
2022-07-18 11:38:03 +0200

    added external link indicator

--- src/_includes/post/meta.11ty.js
+++ src/_includes/post/meta.11ty.js
@@ -4,6 +4,6 @@ module.exports = data => `
 	<time datetime="${new Date(data.post.date).toISOString()}">${new Intl.DateTimeFormat('en-GB', { month: "long", day: 'numeric' , year: 'numeric',}).format(data.post.date)}</time>
 	<div class="tags">
 		${data.tags ? data.tags.filter(tag => tag !== "posts").map(tag => `<a class="tag" href="/tags/${tag.replace(" ", "-").toLowerCase()}/">#${tag}</a>`).join("") : ""}
-	</div>` : "<div>External</div>" }
+	</div>` : "<div>🔗 External</div>" }
 </div>
 `
