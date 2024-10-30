98a5a48c7ba61cbd2a9f5207d0fce629e895dfe4
thoughtsunificator <oss@thoughtsunificator.me>
2024-10-30 03:56:57 +0100

    Add rel=noreferrer and sponsorship link

--- src/about.11ty.js
+++ src/about.11ty.js
@@ -12,18 +12,19 @@ exports.render = function(data) {
 	<section>
 	<h3>Profile</h3>
 	<p>Open 🧙 at thoughtsunificator.me D2D and Web specs is all I care about.</p>
+	<p>And if you ever happen to like my sorcery I kindly ask that you consider <a target="_blank" rel="noreferrer" href="https://github.com/sponsors/thoughtsunificator">becoming a sponsor</a>.</p>
 	<p><a href="/pgp_key.asc">My PGP key.</a></p>
 	</section>
 	<section>
 			<h3>Links</h3>
 			<p>
 			<nav class="links">
-			<div><a target="_blank" href="https://github.com/thoughtsunificator">Github</a></div>
-			<div><a target="_blank" href="https://www.npmjs.com/~thoughtsunificator">npm</a></div>
-			<div><a target="_blank" href="https://addons.mozilla.org/en-US/firefox/user/15968837">AMO</a></div>
-			<div><a target="_blank" href="https://snapcraft.io/publisher/thoughtsunificator">Snapcraft</a></div>
-			<div><a target="_blank" href="https://mastodon.social/@thoughtsunifcator">Mastodon</a></div>
-			<div><a target="_blank" href="https://twitter.com/thoughtsunifier">Twitter</a></div>
+			<div><a rel="noreferrer" target="_blank" href="https://github.com/thoughtsunificator">Github</a></div>
+			<div><a rel="noreferrer" target="_blank" href="https://www.npmjs.com/~thoughtsunificator">npm</a></div>
+			<div><a rel="noreferrer" target="_blank" href="https://addons.mozilla.org/en-US/firefox/user/15968837">AMO</a></div>
+			<div><a rel="noreferrer" target="_blank" href="https://snapcraft.io/publisher/thoughtsunificator">Snapcraft</a></div>
+			<div><a rel="noreferrer" target="_blank" href="https://mastodon.social/@thoughtsunifcator">Mastodon</a></div>
+			<div><a rel="noreferrer" target="_blank" href="https://twitter.com/thoughtsunifier">Twitter</a></div>
 		</nav>
 			</p>
 	</section>
