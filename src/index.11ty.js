4e8bc1246129afdf6fce6af682899b9f1612aba7
thoughtsunificator <oss@thoughtsunificator.me>
2025-01-24 04:34:01 +0100

    Update style

--- src/index.11ty.js
+++ src/index.11ty.js
@@ -13,7 +13,7 @@ export function render(data) {
 	<section id="posts">
 	${posts.map((post, index) => `
 		<section id="post-${index}" class="post">
-		<b><time datetime="${new Date(post.date).toISOString()}">${new Intl.DateTimeFormat('en-GB', { month: "long", day: 'numeric' , year: 'numeric',}).format(post.date)}</time></b>
+		<time datetime="${new Date(post.date).toISOString()}">${new Intl.DateTimeFormat('en-GB', { month: "long", day: 'numeric' , year: 'numeric',}).format(post.date)}</time>
 		<div><a class="title" ${post.data.redirect_to ? ` target="_blank" rel="noopener"` : ""} href="${ post.url }">${post.data.redirect_to ? "#" : ""} ${ post.data.title }</a></div>
 		</section>
 	`).join("")}
